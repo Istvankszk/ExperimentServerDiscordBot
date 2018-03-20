82c6354ec835394239239a272a7aad8b55b4f3e0
Istvankszk <istvan.mentelek@gmail.com>
2018-03-20 21:06:03 +0100

    Added second loop

--- SigmaDiscordBot.sh
+++ SigmaDiscordBot.sh
@@ -2,10 +2,14 @@
 #Purpose = Start and keep alive sigma discord bot
 #Created on 15/03/2018
 #Author = IstvanKSZK
-#Version 1.0
+#Version 1.1
 
 cd ~/SigmaDiscordBot/
-until ~/Python-3.6.4/python SigmaDiscordBot.py > log.txt 2>&1; do #requires python 3.6 on linux
-	mv log.txt log-$`date "+%F-%H-%M"`.txt
-	sleep 1
+while true
+do
+	until ~/Python-3.6.4/python SigmaDiscordBot.py > log.txt 2>&1; do #requires python 3.6 on linux
+		mv log.txt log-$`date "+%F-%H-%M"`.txt
+		sleep 60
+	done
+	sleep 240
 done
